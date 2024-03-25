6a5e9abf2b57c4787a7b6844baae81c305c759bc
johannes.nauta@protonmail.com <johannes.nauta@protonmail.com>
2024-03-25 12:46:55 +0100

    Includes ensemble simulations for SEIRS, includes FHist

--- scripts/SEIRS.jl
+++ scripts/SEIRS.jl
@@ -5,30 +5,78 @@ module SEIRS
 #/ Packages
 using Catalyst
 using DifferentialEquations
+using FHist
 
 #################
 ### FUNCTIONS ###
-function define_DiscreteProblem()
+function define_JumpProblem(
+    ;
+    Nval::Int = 10_000,    
+    U0::Array = ones(Int, 5),
+    βval::Float64 = 1.1094736842,
+    μval::Float64 = 0.2,
+    vval::Float64 = 0.4,
+    wval::Float64 = 0.1,
+    pval::Float64 = 0.9,
+    γval::Float64 = 0.3,
+    tspan = (0.0, 10_000.),
+    kwargs...
+    )
 	  @variables t
     @species S(t) E1(t) E2(t) I(t) R(t)
     @parameters β γ μ p v w N
 
-    #/ Birth
+    #/ Specify reactions
     rxns = [
-        Reaction(μ*N, nothing, [S]),
         Reaction(μ, [S], nothing),
         Reaction(μ, [E1], nothing),
         Reaction(μ, [E2], nothing),
         Reaction(μ, [I], nothing),
         Reaction(μ, [R], nothing),
-        Reaction(β/N, [S, I], [E1, I], [1,1], [1,1]),
-        Reaction(β/N, [S, I], [E2, I], [1,1], [1,1]),
+        Reaction(μ*N, nothing, [S]),
+        Reaction(p*β/N, [S, I], [E1, I], [1,1], [1,1]),
+        Reaction((1-p)*β/N, [S, I], [E2, I], [1,1], [1,1]),
         Reaction(v, [E1], [I]),
         Reaction(w, [E2], [I]),
         Reaction(γ, [I], [R])
     ]
+    #/ Create reaction system
     @named rs = ReactionSystem(rxns, t)
-    return rs
+
+    #/ Specify parameters
+    params = [N => Nval, β => βval, μ => μval, v => vval, w => wval, p => pval, γ => γval]
+    u0 = [S => U0[1], E1 => U0[2], E2 => U0[3], I => U0[4], R => U0[5]]
+    
+    #/ Define DiscreteProblem
+    dprob = DiscreteProblem(rs, u0, tspan, params)
+    jprob = JumpProblem(rs, dprob, Direct(), save_positions=(false,false))
+    return jprob
+end
+
+"Compute the histogram of the steady state abundances of the susceptible population"
+function get_Shist(
+    ;
+    ntrajectories = 50,
+    tspan = (0.0, 5_000.0),
+    kwargs...
+    )
+	  #/ Get EnsembleProblem
+    jprob = define_JumpProblem(; tspan=tspan, kwargs...)
+    output_func(sol, i) = (sol[1,:], false)
+    eprob = EnsembleProblem(jprob, output_func=output_func)
+    #/ Define times at which to save state
+    #~ Note: can also do that within the output func, but do it like this for now
+    tstart = tspan[end] / 4 * 3
+    tpoints = collect(LinRange(tstart, tspan[end], 100))
+    #/ Solve
+    esol = solve(
+        eprob, SSAStepper(), EnsembleThreads(); trajectories=ntrajectories, saveat=tpoints
+    )
+    #/ Collect trajectories and generate histogram
+    _esols = [sol[begin+1:end] for sol in esol]
+    states = reduce(vcat, _esols)
+    fh = FHist.Hist1D(states, 6000:25:8000)
+    return fh |> FHist.normalize
 end
 
 function define_ODEProblem(
@@ -63,15 +111,6 @@ function define_ODEProblem(
 
     prob = ODEProblem(sys, qmap, tspan, pmap)
     return prob
-end
-
-function define_JumpProblem(
-    ;
-    N::Int = 10_000,
-    )
-	  sys = define_system(N)
-    pmap = [β => 0.1]
-    return sys, pmap
 end
 
 end # module SEIRS
